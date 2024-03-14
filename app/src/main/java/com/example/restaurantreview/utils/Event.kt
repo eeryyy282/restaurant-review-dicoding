ad1b5d6fb75b2a32a75852044640feffa186feb9
eeryyy282 <airiagustus82@gmail.com>
2024-03-14 08:39:47 +0700

    add LiveData with Single Event

---
+++ app/src/main/java/com/example/restaurantreview/utils/Event.kt
@@ -0,0 +1,20 @@
+package com.example.restaurantreview.utils
+
+open class Event<out T>(private val content: T) {
+
+    @Suppress("MemberVisibilityCanPrivate")
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    fun peekContent(): T = content
+
+}
